0a776fcb3f27130b36d490dfa6d234fee2771e04
LeO43458 <jlux67079@gmail.com>
2025-09-21 13:39:00 +0200

    Configure 错误日志收集器

---
+++ ErrorLogCollectorApplication_0921_1938_bvy.java
@@ -0,0 +1,12 @@
+// 代码生成时间: 2025-09-21 19:38:59
+package com.example.errorlogcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorLogCollectorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ErrorLogCollectorApplication.class, args);
+    }
+}
